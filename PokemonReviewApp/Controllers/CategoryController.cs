2d63404625dac28465186b048f79c46edb20e259
StarPlatinu <thanhvp1509@gmail.com>
2023-10-22 09:53:10 +0700

    Pokemon Backend

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository,IMapper mapper)
+        {
+         _categoryRepository = categoryRepository;
+         _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var category = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(category);
+        }
+    }
+}
